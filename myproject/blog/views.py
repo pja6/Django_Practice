ed98b5c4c20837ce8bc388734137fefb6a43da5c
Paul <paguilar4@ewu.edu>
2025-02-11 01:35:34 +0000

    created views and url

--- myproject/blog/views.py
+++ myproject/blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Post
+
+def post_list(request):
+    posts = Post.objects.all()  # Get all blog posts from the database
+    return render(request, 'blog/post_list.html', {'posts': posts})
